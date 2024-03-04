4494b1e7cf9c3b3250b760c885f88cab54ee1a15
Persiano99 <sorenrahimi99@gmail.com>
2024-03-04 17:30:41 +0100

    esercizio g1

--- src/Esercizio1.java
+++ src/Esercizio1.java
@@ -2,41 +2,43 @@
 
 public class Esercizio1 {
 
-
-    public static void main(String[] args){
-        int result1 = moltiplica(3,5);
+    public static void main(String[] args) {
+        int result1 = moltiplica(3, 5);
         System.out.println("Risultato della moltiplicazone: " + result1);
 
         String result2 = concatena("Hello", 123);
-        System.out.("Stringa concatenata: " + result2);
-
-        String[] arrayOriginale = {"uno", "due", "tre", "quattro", "cinque" };
+        System.out.println("Stringa concatenata: " + result2);
+        String[] arrayOriginale = {"uno", "due", "tre", "quattro", "cinque"};
         String[] newArray = inserisciInArray(arrayOriginale, "nuovo");
         System.out.print("Nuovo array: ");
-        for (String str: newArray){
-            System.out.print(str +" ");
+        for (String str : newArray) {
+            System.out.print(str + " ");
         }
     }
 
-    public static int moltiplica(int a, int b){
+    public static int moltiplica(int a, int b) {
         return a * b;
     }
 
-    public static String concatena(String str, int num){
+    public static String concatena(String str, int num) {
         return str + num;
     }
 
-    public static String[] inserisciInArray(String[] array, String str){
+    public static String[] inserisciInArray(String[] array, String str) {
         String[] newArray = new String[6];
-        for (int i=0; i<6; i++){
-            if (i<2) {
+        for (int i = 0; i < 6; i++) {
+            if (i < 2) {
                 newArray[i] = array[i];
-            }else if (i == 2){
+            } else if (i == 2) {
                 newArray[i] = str;
-            }else {
+            } else {
                 newArray[i] = array[i - 1];
             }
         }
         return newArray;
     }
 }
+
+
+
+
